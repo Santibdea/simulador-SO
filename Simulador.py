51993291b16953e7d58e0b9de084c1f5ea52d426
Miranda Ariano <arianogabriel02@gmail.com>
2023-10-30 23:22:26 -0300

    Segunda Version

--- Simulador.py
+++ Simulador.py
@@ -11,16 +11,18 @@ lista_proceso = []
 cont = 1
 
 # EL USUARIO DEFINE LA MANERA DE CARGAR EL PROCESO
-print("SELECCIONE COMO QUIERE CARGAR EL PROCESO:")
-print("1 - INGRESAR PROCESO POR TERMINAL")
-print("2 - LEER ARCHIVO CON PROCESOS")
-opcion_carga = int(input())
+rta = 0
+while rta != 1 and rta != 2:
+    print("SELECCIONE COMO QUIERE CARGAR EL PROCESO:")
+    print("1 - INGRESAR PROCESO POR TERMINAL")
+    print("2 - LEER ARCHIVO CON PROCESOS")
+    rta = int(input())
 
-aux = 0
 # SI EL USUARIO INGRESA LOS PROCESOS POR TERMINAL
-if opcion_carga == 1:
+if rta == 1:
     estado_carga = True
     while (cont <= 10 and estado_carga is True):
+        print("------------------------------------------------------")
         print("CARGANDO EL PROCESO:", cont)
         print("INGRESE EL ID DEL PROCESO:")
         id = int(input())
@@ -36,8 +38,8 @@ if opcion_carga == 1:
                 tam_proceso = input()
         print("INGRESE EL TIEMPO DE LLEGADA DEL PROCESO:")
         tiempo_llegada = int(input())
-        if tiempo_llegada <= 0:
-            while (tiempo_llegada <= 0):
+        if tiempo_llegada < 0:
+            while (tiempo_llegada < 0):
                 print("ERROR: TAMAÑO NEGATIVO")
                 tiempo_llegada = int(input())
         print("INGRESE EL TIEMPO DE IRRUPCION DEL PROCESO:")
@@ -48,67 +50,98 @@ if opcion_carga == 1:
                 tiempo_irrupcion = int(input())
         lista_proceso.append(
             [id, tam_proceso, tiempo_llegada, tiempo_irrupcion])
+        print("------------------------------------------------------")
         print("¿DESEA CONTINUAR?")
         print("1 - SI")
         print("2 - NO")
-        opcion_continuar = int(input())
+        rta = 0
         cont += 1
-        if opcion_continuar == 2:
+        while rta != 1 and rta != 2:
+            rta = int(input())
+        if rta == 2:
             estado_carga = False
             cont -= 1
-print(lista_proceso)
 
 # ORDENA POR TIEMPO DE LLEGADA
-cola_procesos = sorted(lista_proceso, key=lambda x: x[2])
-print(cola_procesos)
-
-cola_listos = []
-cola_finalizados = []
-quantum = 2
-particiones = [60, 120, 250]
-
-asignacion = [60, 120, 250]
-fi = [0, 0, 0]
+lista_proceso = sorted(lista_proceso, key=lambda x: x[2])
+print("LISTA ORDENADA:", lista_proceso)
 
 # PRESENTACIONES DE SALIDA
-print("¿DESEA VISUALIZAR LAS PRESENTACIONES DE SALIDA")
-print("1 - SI")
-print("2 - NO")
-opcion_inicio = int(input())
+proceso_en_ejecucion = []
+cola_listos = []
+opcion_seguimiento = True
+t = lista_proceso[0][2]
+quantum = 0
+z = 0
+y = 0
 
-t = 0
-aux = 0
-if opcion_inicio == 1:
-    opcion_seguimiento = True
-    while (opcion_seguimiento is True):
-        if (t == lista_proceso[aux][2]):
-            cola_listos.append(lista_proceso[aux])
-            aux += 1
-            if (cola_listos[0][1] <= 60 and asignacion[0] == 60):
-                asignacion[0] = cola_listos[0][1]
-                fi[0] = 60 - asignacion[0]
-                cola_listos[0] = 0
-                for i in range(0, 8):
-                    cola_listos[i] = cola_listos = [i + 1]
-                    cola_listos[i+1] = 0
-            else:
-                if (cola_listos[0][1] <= 120 and asignacion[1] == 120):
-                    asignacion[1] = cola_listos[0][1]
-                    fi[1] = 120 - asignacion[1]
-                    cola_listos[0] = 0
-                    for i in range(0, 8):
-                        cola_listos[i] = cola_listos = [i + 1]
-                        cola_listos[i+1] = 0
-                else:
-                    if (cola_listos[0][1] <= 150 and asignacion[2] == 150):
-                        asignacion[2] = cola_listos[0][1]
-                        fi[2] = 150 - asignacion[2]
-                        cola_listos[0] = 0
-                        for i in range(0, 8):
-                            cola_listos[i] = cola_listos = [i + 1]
-                            cola_listos[i+1] = 0
-    print("¿DESEA CONTINUAR?")
-    print("1 - SI")
-    print("2 - NO")
-    if opcion_continuar == 2:
+while opcion_seguimiento is True:
+    # SE PREGUNTA SI NO HAY MAS PROCESOS EN COLA DE LISTOS Y EN LISTA DE PROCESOS:
+    if (len(cola_listos) == 0 and len(lista_proceso) == 0):
         opcion_seguimiento = False
+        # SI HAY UN PROCESO EJECUTANDOSE:
+        # SE PREGUNTA SI YA SE TERMINO DE EJECUTAR EL PROCESO
+    if len(proceso_en_ejecucion) != 0:
+        if proceso_en_ejecucion[0][3] == 0:
+            proceso_en_ejecucion = []
+            if (len(proceso_en_ejecucion) == 0 and len(cola_listos) == 0 and len(lista_proceso) == 0):
+                y = 1
+            quantum = 0
+
+    # CARGA DE PROCESOS A COLA DE LISTOS
+    if len(lista_proceso) != 0:
+        while t == lista_proceso[0][2]:
+            cola_listos.append(lista_proceso[0])
+            tamaño = len(lista_proceso) - 1
+            lista_proceso[0] = [0, 0, 0, 0]
+            for i in range(0, tamaño):
+                lista_proceso[i] = lista_proceso[i+1]
+                lista_proceso[i+1] = 0
+            # UNA VEZ SE PASA UN PROCESO A COLA DE LISTOS, SE ELIMINA DICHO PROCESO DE ESTA LISTA
+            lista_proceso.pop(tamaño)
+            if len(lista_proceso) == 0:
+                lista_proceso.append([0, 0, 0, 0])
+                z = 1
+
+    if z == 1:
+        lista_proceso.pop(0)
+    z = 0
+
+    if quantum == 2:
+        if (len(proceso_en_ejecucion) != 0 and y == 0):
+            cola_listos.append(proceso_en_ejecucion[0])
+            proceso_en_ejecucion.pop(0)
+        quantum = 0
+
+    # SE PREGUNTAR SI SE PUEDE EJECUTAR UN PROCESO
+    if (len(proceso_en_ejecucion) == 0 and y == 0):
+        proceso_en_ejecucion.append(cola_listos[0])
+        # MOVEMOS UNA POSICION A LA IZQUIERDA CADA PROCESO DE LA COLA DE LISTOS
+        tamaño = len(cola_listos) - 1
+        for i in range(0, tamaño):
+            cola_listos[i] = cola_listos[i+1]
+            cola_listos[i+1] = 0
+        cola_listos.pop(tamaño)
+    if y == 0:
+        print("----------------------------------------------------------")
+        print("INSTANTE:", t)
+        print("ESTADO DEL PROCESADOR:", proceso_en_ejecucion)
+        print("COLA DE LISTOS:", cola_listos)
+        print("----------------------------------------------------------")
+        t += 1
+        quantum += 1
+        proceso_en_ejecucion[0][3] = proceso_en_ejecucion[0][3] - 1
+
+    if y == 0:
+        print("¿DESEA CONTINUAR?")
+        print("1 - SI")
+        print("2 - NO")
+        rta = 0
+        while rta != 1 and rta != 2:
+            rta = int(input())
+        if rta == 2:
+            opcion_seguimiento = False
+    else:
+        opcion_seguimiento = False
+
+print("FIN")
