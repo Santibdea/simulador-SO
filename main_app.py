8cd94868768abd4f1acfbb6d02e8a15b9a78444f
Santiago Beron <santiagobdea@gmail.com>
2023-11-05 15:02:18 -0300

    Procesamiento correcto con quantum = 2

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
+from collections import deque
 
 # Sets the appearance mode of the application
 # "System" sets the appearance same as that of the system
@@ -11,6 +12,8 @@ ctk.set_appearance_mode("Dark")
 # Sets the color of the widgets
 # Supported themes: green, dark-blue, blue
 ctk.set_default_color_theme("dark-blue")
+
+#Se deberia definir el estado del proceso por la cola en la cual se encuentra.
 
 # Estados posibles de un proceso: ready, ready and suspended, in execution, y finished
 
@@ -26,10 +29,11 @@ class ComputerProcess:
 
 
 class Partition:
-    def __init__(self, size, not_busy=True):
+    def __init__(self, partition_id, size, proccess_asigned = None,):
+        self.partition_id = partition_id
         self.size = size
-        self.not_busy = not_busy
-
+        self.proccess_asigned = proccess_asigned
+        self.intern_fragmentation = 0
 
 class Simulator:
     def __init__(self, master):
@@ -37,12 +41,14 @@ class Simulator:
         self.master.title(
             "Simulador de Asignación de Memoria y Planificación de Procesos")
         self.process_ids = 1
-        self.memory_partitions = [Partition(250), Partition(
-            120), Partition(60), Partition(100)]
-        self.ready_queue = []
-        self.titles = ['Memory Partition', 'Process ID',
-                       'Process Size', 'Arrival Time', 'Execution Time']
+        OS = ComputerProcess(0, 100, 0, 0, )
+        self.memory_partitions = [Partition(1, 250), Partition(2, 120), Partition(3, 60), Partition(4, 100, OS)]
+        self.ready_queue = deque()
+        self.proccess_in_execution = None
+        self.titles = ['Memory Partition', 'Process ID', 'Process Size', 'Arrival Time', 'Execution Time']
         self.quantum = 2
+        self.all_proccess = []
+        self.finished_queue = []
         self.total_execution = 0
         self.total_execution_inverse = 0
 
@@ -112,18 +118,23 @@ class Simulator:
         process_size = self.process_size_entry.get()
         process_ta = self.process_ta_entry.get()
         process_ti = self.process_ti_entry.get()
+        print(process_size)
+        print(process_ta)
+        print(process_ti)
+
         if not process_ta or not process_size or not process_ti:
             messagebox.showerror(
                 "Error", "Por favor, ingrese el tiempo de arribo, el tiempo de irrupción y el tamaño del proceso.")
             return
+        
+
 
         self.process_ta_entry.delete(0, tk.END)
         self.process_ti_entry.delete(0, tk.END)
         self.process_size_entry.delete(0, tk.END)
 
         try:
-            self.save_process(self, int(process_size),
-                              int(process_ta), int(process_ti))
+            self.save_process(int(process_size), int(process_ta), int(process_ti))
         except ValueError:
             messagebox.showerror(
                 "Error", "El tamaño del proceso debe ser un número entero.")
@@ -140,13 +151,17 @@ class Simulator:
                 self.save_process(line[0], line[1], line[2])
 
     def save_process(self, p_size, p_ta, p_ti):
+        if len(self.all_proccess) == 10:
+            messagebox.showerror(
+                "Error", "No es posible cargar mas de 10 procesos")
+            return
         process_id = self.process_ids
         process = ComputerProcess(process_id, p_size, p_ta, p_ti)
         self.total_execution += int(p_ti)
-        self.ready_queue.append(process)
+        self.all_proccess.append(process)
         self.process_ids += 1
         self.ready_queue_listbox.insert(
-            tk.END, f"ID: {process.id}, TA: {process.arrival_time}, TI: {process.execution_time}\n")
+            tk.END, f"ID: {process.id}, TA: {process.arrival_time}, TI: {process.execution_time}, TAMAÑO: {p_size} \n")
 
     def delete_buttons(self):
         # Destroy the button
@@ -170,7 +185,7 @@ class Simulator:
 
     def load_to_memory(self):
         matrix = self.matrix
-        for i in range(0, len(self.ready_queue)):
+        for i in range(0, len(self.f)):
             if i < self.multiprog_degree:
                 process = self.ready_queue[i]
                 if i < len(self.memory_partitions) - 1:
@@ -198,14 +213,39 @@ class Simulator:
 
     def start_simulation(self):
         # Ordenar por tiempo de arribo:
-        self.ready_queue = sorted(
-            self.ready_queue, key=lambda x: x.arrival_time)
+        # self.ready_queue = sorted(
+        #     self.ready_queue, key=lambda x: x.arrival_time)
 
         # Limpiar lo ingresado y reordenarlo por tiempo de arribo
+
+        self.all_proccess = sorted(self.all_proccess, key=lambda proceso: (proceso.id, proceso.arrival_time))
+        all_proccess_copy = self.all_proccess.copy()
+
+        for proceso in all_proccess_copy:
+            print(f"proceso antes {proceso.id}")
+
+        for proceso in all_proccess_copy:
+            print(f"proceso {proceso.id}")
+            if len(self.ready_queue) < 5:
+                self.ready_queue.append(proceso)
+                self.all_proccess.remove(proceso)
+            else:
+                break
+
+
+        # for process in self.all_proccess:
+        #     print(f"pasa el proceso {process.id}")
+        #     self.ready_queue.append(process)
+        #     self.all_proccess.remove(process)
+        #     self.ready_queue_listbox.insert(
+        #     tk.END, f"ID: {process.id}, TA: {process.arrival_time}, TI: {process.execution_time}\n")
+        
         self.ready_queue_listbox.delete("1.0", "end")
-        for queue_process in self.ready_queue:
-            self.ready_queue_listbox.insert(
-                tk.END, f"ID: {queue_process.id}, TA: {queue_process.arrival_time}, TI: {queue_process.execution_time}\n")
+
+        # Codigo viejo
+        # for queue_process in self.ready_queue:
+        #     self.ready_queue_listbox.insert(
+        #         tk.END, f"ID: {queue_process.id}, TA: {queue_process.arrival_time}, TI: {queue_process.execution_time}\n")
 
         # Reutilizar variable. Ahora representa la row de la lista de finalizados
         self.process_ids = 1
@@ -214,6 +254,12 @@ class Simulator:
         matrix.grid(row=1, column=0)
 
         cola_finalizados = self.cola_finalizados
+
+        for proceso in self.ready_queue:
+            print(f"proceso id {proceso.id}")
+
+        self.proccess_in_execution = self.ready_queue.popleft() #Inicio el primer proceso
+
         cola_finalizados.grid(row=1, column=1, sticky="ne")
 
         self.delete_buttons()
@@ -221,11 +267,18 @@ class Simulator:
         self.right_frame = tk.Frame(
             self.bottom_frame, bg="#00448B", borderwidth=10)
         self.right_frame.grid(row=1, column=3, padx=5, pady=5)
-
-        ctk.CTkLabel(self.right_frame, text="Proceso en ejecución").grid(
+        
+        self.proccess_in_execution_label = ctk.CTkLabel(self.right_frame, text="Proceso en ejecución").grid(
             row=0, column=0, padx=10, pady=10,)
-        ctk.CTkLabel(self.right_frame, text="Proceso que salió de ejecución").grid(
+        
+        
+        self.proccess_exit = ctk.CTkLabel(self.right_frame, text="Proceso que salió de ejecución").grid(
             row=0, column=1, padx=10, pady=10)
+        
+        self.procces_enter = ctk.CTkLabel(self.right_frame, text="Proceso que se asigno a ejecucion").grid(
+            row=0, column=2, padx=10, pady=10)
+        
+        
 
         for index, partition in enumerate(self.memory_partitions):
 
@@ -242,15 +295,17 @@ class Simulator:
                                                 column=index+1, padx=5, pady=5)
         ctk.CTkLabel(matrix, text=self.titles[4]).grid(
             row=0, column=4, padx=5, pady=5)
-        self.load_to_memory()
+        # self.load_to_memory()
 
         ctk.CTkLabel(cola_finalizados, text="Procesos finalizados").grid(
             row=0, padx=100, pady=5)
 
         ctk.CTkButton(self.bottom_frame, text="Finalizar Simulación",
                       command=self.finish_simulation).grid(row=2, column=0, pady=10)
+        
         ctk.CTkButton(self.bottom_frame, text="Continuar simulación",
                       command=self.continue_simulation).grid(row=2, column=1, pady=10)
+        
         self.progress_bar = ctk.CTkProgressBar(
             self.master, orientation="horizontal")
         self.progress_bar.grid(row=9, pady=10, columnspan=2)
@@ -290,39 +345,128 @@ class Simulator:
         ctk.CTkLabel(self.right_frame, text=self.ready_queue[0].id).grid(
             row=1, column=0)
 
-    def continue_simulation(self):
-        if self.ready_queue:
-            self.total_execution_inverse += 1
-            self.progress_bar.set(
-                self.total_execution_inverse / self.total_execution)
-            self.quantum -= 1
-            self.tiempo_actual = ctk.CTkLabel(self.master, text=str(self.total_execution_inverse)).grid(
-                row=10, pady=10, column=1)
+    def continue_simulation(self, ):
 
-            process = self.ready_queue[0]
-            # Se almacena como string porque para mostrarlo en la ventana necesita ser string
-            process.execution_time = str(int(process.execution_time) - 1)
+        
+        if self.proccess_in_execution is not None:
+                
+                for i in range(1, 3):
+                    self.total_execution_inverse += 1
+                    self.progress_bar.set(self.total_execution_inverse / self.total_execution)
+                    self.tiempo_actual = ctk.CTkLabel(self.master, text=str(self.total_execution_inverse)).grid(row=10, pady=10, column=1)
+                    tiempo_restante = self.proccess_in_execution.execution_time - 1
+                    self.proccess_in_execution.execution_time = tiempo_restante
 
-            if process.execution_time == '0':  # '0' no es falsy >:(
-                self.end_process()
-                self.load_to_memory()
-                self.process_ids += 1
-                self.quantum = self.max_quantum
+                    # process.execution_time = str(int(process.execution_time) - 1)
+                    # if process.execution_time == 0:  # '0' no es falsy >:(
+                    #     self.end_process()
+                    #     self.load_to_memory()
+                    #     self.process_ids += 1
+                    #     self.quantum = self.max_quantum
 
-            else:
-                ctk.CTkLabel(self.matrix, text=process.execution_time).grid(
-                    row=process.location+1, column=4, padx=5, pady=5)
-                if not self.quantum:
-                    self.quantum = self.max_quantum
-                    ctk.CTkLabel(self.right_frame, text=process.id).grid(
-                        row=1, column=1)
-                    self.next_execution()
-                    process = self.ready_queue[0]
-                    ctk.CTkLabel(self.right_frame, text=process.id).grid(
-                        row=1, column=0)
+                    # else:
+                    #     ctk.CTkLabel(self.matrix, text=process.execution_time).grid(
+                    #         row=process.location+1, column=4, padx=5, pady=5)
+                    #     if not self.quantum:
+                    #         self.quantum = self.max_quantum
+                    #         ctk.CTkLabel(self.right_frame, text=process.id).grid(
+                    #             row=1, column=1)
+                    #         self.next_execution()
+                    #         process = self.ready_queue[0]
+                    #         ctk.CTkLabel(self.right_frame, text=process.id).grid(
+                    #             row=1, column=0)
 
-        else:
-            self.finish_simulation()
+                # else:
+                #     self.finish_simulation()
+                    if self.proccess_in_execution.execution_time == 0 or i == 2:
+                        # tiempo_finalizacion = self.tiempo
+                        # tiempo_retorno = tiempo_finalizacion - self.proceso_en_ejecucion.tiempo_arribo
+                        # tiempo_espera = tiempo_retorno - self.proceso_en_ejecucion.tiempo_irrupcion   # Para estadisticas
+                        
+                        if self.proccess_in_execution.execution_time == 0:   
+                            # Aqui si el proceso termina se va a la cola de finalizados
+                            self.finished_queue.append(self.proccess_in_execution) 
+                            print(f"El proceso {self.proccess_in_execution.id} termino") 
+                            proceso_actual_id = self.proccess_in_execution.id if self.proccess_in_execution else "Ninguno"
+
+                            self.proccess_exit = ctk.CTkLabel(self.right_frame, text="Proceso que abandono el procesador: {}".format(proceso_actual_id))
+                            self.proccess_exit.grid(row=0, column=1, padx=10, pady=10)
+                        
+                            self.proccess_in_execution = None
+
+                            if self.ready_queue: # Si hay procesos en cola de listos, se asigna el siguiente nuevamente
+                                proceso_siguiente = self.ready_queue.popleft()
+                                self.proccess_in_execution = proceso_siguiente
+                                self.procces_enter = ctk.CTkLabel(self.right_frame, text="Proceso que se asigno al procesador: {}".format(proceso_siguiente.id))           
+                                self.procces_enter.grid(row=0, column=2, padx=10, pady=10)    
+                            break
+                        
+                        if i==2 and self.proccess_in_execution.execution_time != 0 :
+                            # Aquí se devuelve el proceso en ejecución a la cola de listos
+                            proceso_devuelto = self.proccess_in_execution
+                            self.ready_queue.append(proceso_devuelto)
+                            proceso_actual_id = self.proccess_in_execution.id if self.proccess_in_execution else "Ninguno"
+
+                            self.proccess_exit = ctk.CTkLabel(self.right_frame, text="Proceso que abandono el procesador: {}".format(proceso_actual_id))
+                            self.proccess_exit.grid(row=0, column=1, padx=10, pady=10)
+                        
+                            self.proccess_in_execution = None
+
+                            if self.ready_queue: # Si hay procesos en cola de listos, se asigna el siguiente nuevamente
+                                proceso_siguiente = self.ready_queue.popleft()
+                                self.proccess_in_execution = proceso_siguiente
+                                self.procces_enter = ctk.CTkLabel(self.right_frame, text="Proceso que se asigno al procesador: {}".format(proceso_siguiente.id))           
+                                self.procces_enter.grid(row=0, column=2, padx=10, pady=10)  
+                            break
+
+
+
+                        # Ya sea por el quantum o porque termino, debe salir del procesador.
+                             
+                        # while len(simulador.cola_procesos_listos) < 5 and lista_procesos_ordenados: #Controlo multiprogramacion
+                        #     proceso_pendiente = lista_procesos_ordenados.pop(0)
+                        #     simulador.cola_procesos_listos.append(proceso_pendiente)
+
+                
+                
+                # Imprimir el tiempo actual y el proceso en ejecución (o "Ninguno" si no hay proceso)
+                proceso_actual_id = self.proccess_in_execution.id if self.proccess_in_execution else "Ninguno"
+                self.proccess_in_execution_label = ctk.CTkLabel(self.right_frame, text="Proceso en ejecución {}".format(proceso_actual_id))
+                self.proccess_in_execution_label.grid(row=0, column=0, padx=10, pady=10,)
+
+        
+        # if self.ready_queue:
+        #     self.total_execution_inverse += 1
+        #     self.progress_bar.set(
+        #         self.total_execution_inverse / self.total_execution)
+            
+        #     self.tiempo_actual = ctk.CTkLabel(self.master, text=str(self.total_execution_inverse)).grid(
+        #         row=10, pady=10, column=1)
+
+        #     process = self.ready_queue[0]
+        #     # Se almacena como string porque para mostrarlo en la ventana necesita ser string
+        #     process.execution_time = str(int(process.execution_time) - 1)
+
+        #     if process.execution_time == '0':  # '0' no es falsy >:(
+        #         self.end_process()
+        #         self.load_to_memory()
+        #         self.process_ids += 1
+        #         self.quantum = self.max_quantum
+
+        #     else:
+        #         ctk.CTkLabel(self.matrix, text=process.execution_time).grid(
+        #             row=process.location+1, column=4, padx=5, pady=5)
+        #         if not self.quantum:
+        #             self.quantum = self.max_quantum
+        #             ctk.CTkLabel(self.right_frame, text=process.id).grid(
+        #                 row=1, column=1)
+        #             self.next_execution()
+        #             process = self.ready_queue[0]
+        #             ctk.CTkLabel(self.right_frame, text=process.id).grid(
+        #                 row=1, column=0)
+
+        # else:
+        #     self.finish_simulation()
 
     def finish_simulation(self):
         # custom_box = tk.Toplevel(root)
